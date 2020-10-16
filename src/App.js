a58bbbe5a26ea5d50fbf9680a2617bf5d62ee66f
chaikovskyit <chaikovskyit@gmail.com>
2020-10-16 11:06:11 +0300

    create hoc component Layout and create style for him

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
-import Quiz from './containers/Quiz/Quiz';
 import Layout from './hoc/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Layout>
-          <Quiz /> 
-        </Layout>  
-      </div>
+      <Layout>
+      {/* Тут буде складено увесь наш роутинг */}
+
+
+        <div style={{width: 400, border: '1px solid black'}}>
+          <h1>Layout</h1>
+        </div>
+      </Layout>
     )
   }
 }
--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
-import classes from './Layout.module.css'
-
 
 class Layout extends Component {
   render() {
     return(
-      <div className={classes.Layout}>
+      // Це корріний дів проекту
+      <div>
         <main>
+          {/* Тут буде весь контент */}
           {this.props.children}
         </main>
       </div>
@@ -14,4 +14,4 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+export default Layout
