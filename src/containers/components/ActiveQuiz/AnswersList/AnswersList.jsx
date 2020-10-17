3f60a783fd22a22063caf98e5c05fa84b269c7b5
chaikovskyit <chaikovskyit@gmail.com>
2020-10-17 21:17:05 +0300

    add coments for AnswersList.jsx

--- src/containers/components/ActiveQuiz/AnswersList/AnswersList.jsx
+++ src/containers/components/ActiveQuiz/AnswersList/AnswersList.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import classes from './AnswersList.module.css'
 import AnswerItem from './AnswerItem/AnswerItem'
 
+// Компонент що відповідає за відображення списук варіантів відповідей
 const AnswersList = (props) => {
   return (
     <ul className={classes.AnswersList}>
+      {/* Методом меп ми проходимось по масиву варіантів відповідей який знаходиться в стейті, і додавши до них індекс малюєио їх у нашому лісті */}
       {props.answers.map((answer, index) => {
         return (
           <AnswerItem 
+            // індекс елементу типу ідентифікатора
             key={index}
+            // елемент
             answer={answer}
+            // функція кліку
             onAnswerClick={props.onAnswerClick}
           />
         )
