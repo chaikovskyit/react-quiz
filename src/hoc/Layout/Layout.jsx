f7a1eccbc0e25091b49a8ed938e70107747d7820
chaikovskyit <chaikovskyit@gmail.com>
2020-10-20 12:47:34 +0300

    create new component MenuToggle

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,12 +1,28 @@
 import React, {Component} from 'react'
 import classes from './Layout.module.css'
-import Quiz from '../../containers/Quiz/Quiz'
+import MenuToggle from '../../containers/components/Navigation/MenuToggle/MenuToggle'
+
 
 class Layout extends Component {
+
+  state = {
+    menu: false,
+  }
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+
   render() {
     return(
-      // Це корріний дів проекту
+      // Це корріний <div/> проекту
       <div className={classes.Layout}>
+        <MenuToggle 
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
         <main>
           {/* Тут буде весь контент */}
           {this.props.children}
