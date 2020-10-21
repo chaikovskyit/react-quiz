4cdb1a7b4f50ad6e02a7e2dbcf39df3db5cda2a3
chaikovskyit <chaikovskyit@gmail.com>
2020-10-21 17:06:54 +0300

    install react-router-dom, and wrap App component in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// імпортуємо роутер, а перед тим встановлюємо пакет командою npm add react-router-dom
+import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// створюємо зміну
+const app  = (
+  // тепер огортаємо увесь наш додаток в даний компонент
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+// в ReactDOM.render складуєм змінну app
+ReactDOM.render(<app />, document.getElementById('root'));
 
 serviceWorker.unregister();
