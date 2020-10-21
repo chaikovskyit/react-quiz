e8d62178738c51c9069e47f8a7539f9fbfff1ae1
chaikovskyit <chaikovskyit@gmail.com>
2020-10-21 17:11:48 +0300

    fixed bag

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // імпортуємо роутер, а перед тим встановлюємо пакет командою npm add react-router-dom
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 // створюємо зміну
@@ -14,6 +14,6 @@ const app  = (
   </BrowserRouter>
 )
 // в ReactDOM.render складуєм змінну app
-ReactDOM.render(<app />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 
 serviceWorker.unregister();
