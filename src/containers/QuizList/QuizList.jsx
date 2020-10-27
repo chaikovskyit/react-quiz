f4069d65528df49c91502337ade809cdf681a7d5
chaikovskyit <chaikovskyit@gmail.com>
2020-10-27 14:36:42 +0200

    get request to server and render quizes array

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -6,24 +6,54 @@ import axios from 'axios'
 // Компонент який відповідає за список доступних тестів і навігацію між ними
 class QuizList extends Component {
   
+  state = {
+    // Масив в який з сервера завантажуються наші тести
+    quizes: []
+  }
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    // За допомогою методу "map()" пробігаємось по оновленому масиві і рендеремо кожен його.
+    return this.state.quizes.map((quiz) => {
       return(
         <li
-          key={index}
+
+          key={quiz.id}
         >
-          <NavLink to={'/quiz/' + quiz}>
-            Тест {quiz}
+          <NavLink to={'/quiz/' + quiz.id}>
+            {quiz.name}
           </NavLink>
         </li>
       )
     })
   }
 
-  componentDidMount() {
-    axios.get('https://react-quiz-52bd0.firebaseio.com/quiz.json').then(response => {
-      console.log(response);
-    })
+  // Працювати з сервером ми можемо тоді коли в нас є DOM дерево, бо якщо його нема то ми не можемо змінювати "state" бо в нас полізуть помилки. Тому в 99% випадків для того щоб зробити запит на сервер ми використовуватимемо метод "componentDidMount()". Який каже нам про те що дерефо сформувалось що робимо далі? Тепер якщо в нас функція стрілочна то ми "async" пишемо в перед параметрами, а якщо це ES6 то пишемо ось так  
+  async componentDidMount() {
+    try {
+      // cтворюємо змінну " response ", І в ній звертаємось до axios і кажемо нас цікавить метод "get"(дай мені те що лежить на сервері в файлі "quizes.json" )
+      const response = await axios.get('https://react-quiz-52bd0.firebaseio.com/quizes.json')
+      // локальна змінна (масив) в який ми додаємо тести
+      const quizes = []
+      // дивимось що ми отримуємо з сервера, в "response.data" ми отримуємо обєкт в якому ключі являються "id" потрібного нам тесту "Quiz ID =  -MKdlqBUmPaSFmlAfe6F" з його даними. Тепер нам просто потрібно переформатувати дані в той формат який нам підходить.
+      console.log(response.data);
+      // Для цього юзаємо оператор "Object.keys" для того щоб пробігтись по масиву і за допомогою методу  "forEach()" будемо отримувати ключ даного ключа, тобто "id" кожного обєкту і "index". Тепер нам треба сформувати "state" для даного компоненту де в нас буде зберігатись масив списку тестів які ми завантажуватимемо з сервера
+      Object.keys(response.data).forEach((key, index) => {
+        // на кожній ітерації ми додаємо новий тест в наш масив
+        quizes.push({
+          // ключ який ми отримали з сервера
+          id: key,
+          // імя тесту яке відображатиметься в компоненті <QuizList/>
+          name: `Тест №${index + 1}`
+        })
+      })
+      // Ми напакували наш локальний масив, тепер проводимо операцію зміни "state"
+      this.setState({
+        quizes: quizes
+      })
+      // ловить помилку
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   render(){
