56ad08ad3cfed4cc9859de5bd42328a06368c623
chaikovskyit <chaikovskyit@gmail.com>
2020-10-31 16:31:10 +0200

    add redux-thunk, create store, actionTypes, rootReducer

--- src/index.js
+++ src/index.js
@@ -5,13 +5,45 @@ import App from './App';
 // імпортуємо роутер, а перед тим встановлюємо пакет командою npm add react-router-dom
 import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+// імпортуємо функцію "createStore" для створення "store", і "compose" для "DevTools"
+import {createStore, compose, applyMiddleware} from 'redux'
+// імпортуємо hoc "Provider" для того щоб огорнути наш додаток
+import {Provider} from 'react-redux'
+// підключаємо наш rootReducer і передаємо його в якості параметра в функцію "createStore()"
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+
+// Підключаємо "DevTools"
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+
+// Створюємо "store"
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+  
+  )
+
+
 
 // створюємо зміну
 const app  = (
-  // тепер огортаємо увесь наш додаток в даний компонент
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  // огортаємо наш додаток в <Provider/> і передаємо йому "store"
+  <Provider store={store}>
+    {/* тепер огортаємо увесь наш додаток в <BrowserRoute/> компонент */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  
 )
 // в ReactDOM.render складуєм змінну app
 ReactDOM.render(app, document.getElementById('root'));
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+// Це функція обєднююча усі наші reducers
+
+import {combineReducers} from 'redux'
+
+export default combineReducers({
+  // Сюди передаємо обєкт конфігурації де будуть описанні усі наші reducers
+})
