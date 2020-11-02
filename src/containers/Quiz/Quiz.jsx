4234878c84fdbff3dac90efdc117fcb3922e3252
chaikovskyit <chaikovskyit@gmail.com>
2020-11-02 14:31:50 +0200

    refactoring Quiz finish

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -2,139 +2,19 @@ import React, {Component} from 'react'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 import classes from './Quiz.module.css'
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
-import axios from '../../axios/axios-quiz'
 import Loader from '../../components/UI/Loader/Loader'
 import {connect} from 'react-redux'
-import {fetchQuizById} from '../../store/actions/quiz'
+import {fetchQuizById, quizAnswerClick, retryQuiz} from '../../store/actions/quiz'
 
 
 class Quiz extends Component {
-  // state = {
-  //   // //обєкт в якому збираються результати відповідей на запитання
-  //   // // { [id]: 'success' or 'error' }
-  //   // results: {},
-  //   // // Відповідає за поточний стан вікторини. За замовчуванням це false тобто вона не завершена, а коли буде дано відповідь на останнє запитання значення isFinished зміниться на true.
-  //   // isFinished: false,
-  //   // // Поточний номер запитання 
-  //   // activeQuestion: 0,
-  //   // // Зберігає інформацію про поточний клік користувача(відповідь або правильна або неправильна {[id]: 'success' 'error'}  )
-  //   // answerState: null,
-  //   // // добавляємо стан лоадера
-  //   // loading: true,
-  //   // quiz: [
-  //   //   // {
-  //   //   //   // Питання 
-  //   //   //   question: 'Що таке ReactJS',
-  //   //   //   // id правельної відповіді
-  //   //   //   rightAnswerId: 2,
-  //   //   //   // id запитання, тобто порядковий номер запитання
-  //   //   //   id: 1,
-  //   //   //   // Тестові варіанти відповідей з id для зручності маніпуляцій з ними
-  //   //   //   answers: [
-  //   //   //     {text: 'Framework', id: 1},
-  //   //   //     {text: 'Бібліотека', id: 2},
-  //   //   //     {text: 'Мова програмування', id: 3},
-  //   //   //     {text: 'Супер Герой', id: 4}
-  //   //   //   ]
-  //   //   // },
-  //   //   // {
-  //   //   //   // Питання 
-  //   //   //   question: 'Хто такий Дарт Вейдер',
-  //   //   //   // id правельної відповіді
-  //   //   //   rightAnswerId: 3,
-  //   //   //   // id запитання, тобто порядковий номер запитання
-  //   //   //   id: 2,
-  //   //   //   // Тестові варіанти відповідей з id для зручності маніпуляцій з ними
-  //   //   //   answers: [
-  //   //   //     {text: 'Супер герой', id: 1},
-  //   //   //     {text: 'Yoda', id: 2},
-  //   //   //     {text: 'Енакен Скай Вокер', id: 3},
-  //   //   //     {text: 'Палпатін', id: 4}
-  //   //   //   ]
-  //   //   // }
-  //   // ]
-  // }
-  // Функція яка виводить в консолі id елементу зі списку варіантів по якому був зроблений клік, її передаємо через пропси в сам низ до елементу AnswerItem де вона і викликається.
-  onAnswerClickHandler = (answerId) => {
-    console.log('Answer id:', answerId);
-    if(this.state.answerState) {
-      const key = Object.keys(this.state.answerState)[0]
-      if(this.state.answerState[key] === 'success'){
-        return 
-      }
-    }
-
-    // Тут лежить питання
-    const question = this.state.quiz[this.state.activeQuestion]
-    const results = this.state.results
-
-    // Тут ми перевіряємо чи правильно ми відповіли на запитання
-    if(question.rightAnswerId === answerId) {
-      // якщо відповідь правильна то results буде дорівнювати 'success'
-      if(!results[question.id]) {
-        results[question.id] = 'success'
-      }
-      this.setState({
-        answerState: {[answerId]: 'success'},
-        //// В обєкт results додаємо значення зміної results в даному випадку 'success', тобто змінюємо state
-        results: results
-      })
-      const timeout = window.setTimeout(() => {
-        if(this.isQuizFinished()) {
-          // коли вікторина завершена значення в state змінюється на true
-          this.setState({
-            isFinished: true
-          })
-        } else {
-          // При кліку змінюємо activeQuestion якшо відповідь правильна
-          this.setState({
-            activeQuestion: this.state.activeQuestion +1,
-            answerState: null
-          })
-        }
-        window.clearTimeout(timeout)
-      }, 1000)
-    } else {
-      // якщо відповідь не правильна, змінній results присвоюється значення 'error'
-      results[question.id] = 'error'
-      this.setState({
-        answerState: {[answerId]: 'error'},
-        // В обєкт results додаємо значення зміної results в даному випадку 'error', тобто змінюємо state
-        results: results
-        
-      })
-    }
-  }
-  isQuizFinished() {
-    return this.state.activeQuestion + 1 === this.state.quiz.length
-  }
-  // Функція яка повертає вікторину в початкове значення, тобто обнулює всі показники і ми можемо проходити вікторину заново. Обнуляємо state
-  retryHandler = () => {
-    this.setState({
-      activeQuestion: 0,
-      answerState: null,
-      isFinished: false,
-      results: {},
-    })
-  }
-  // ?
-  // даємо запит на сервер, для того щоб отримати наші тести
-  // async componentDidMount() {
-  //   try {
-  //     const response = await axios.get(`/quizes/${this.props.match.params.id}.json`)
-  //     const quiz = response.data
-
-  //     this.setState({
-  //       quiz: quiz,
-  //       loading: false
-  //     })
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-    
-  // }
   componentDidMount() {
     this.props.fetchQuizById(this.props.match.params.id)
+  }
+
+  // викликається тоді коли компонент знищується
+  componentWillUnmount() {
+    this.props.retryQuiz()
   }
 
   render() {
@@ -142,7 +22,6 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Дайте відповідь на запитання</h1>
-
           {
             this.props.loading || !this.props.quiz
               ? <Loader />
@@ -152,12 +31,12 @@ class Quiz extends Component {
                     results={this.props.results}
                     quiz={this.props.quiz}
                     // Викликаємо функцію пройти вікторину заново
-                    onRetry={this.retryHandler}
+                    onRetry={this.props.retryQuiz}
                   />
                 : <ActiveQuiz 
                     answers={this.props.quiz[this.props.activeQuestion].answers}
                     question={this.props.quiz[this.props.activeQuestion].question}
-                    onAnswerClick={this.onAnswerClickHandler}
+                    onAnswerClick={this.props.quizAnswerClick}
                     // Параметр який відповідає загальну кількість запитаня
                     quizLength={this.props.quiz.length}
                     // Параметр який відповідає за номер поточного запитання
@@ -166,10 +45,6 @@ class Quiz extends Component {
                   />
                   // {/* Відповідно до значення state залежить те який компонент ми будемо рендерити, по дефолту ми рендиремо ActiveQuiz, а коли значення в state змінюється на true ми рендиремо FinishedQuiz */}
           }
-
-
-            
-          
         </div>
       </div>
     )
@@ -189,8 +64,9 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetchQuizById: (id) => dispatch(fetchQuizById(id))
-
+    fetchQuizById: (id) => dispatch(fetchQuizById(id)),
+    quizAnswerClick: answerId => dispatch(quizAnswerClick(answerId)),
+    retryQuiz: () => dispatch(retryQuiz())
   }
 }
 
--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,6 +1,6 @@
 // reducer який відповідає за тести
 
-import { FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZ_SUCCESS } from "../actions/actionTypes"
+import { FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZ_SUCCESS, QUIZ_SET_STATE, FINISH_QUIZ, QUIZ_NEXT_QUESTION, QUIZ_RETRY} from "../actions/actionTypes"
 
 const initialState = {
   quizes: [],
@@ -41,6 +41,35 @@ export default function quizReducer(state = initialState, action) {
         ...state, 
         loading: false,
         quiz: action.quiz
+      } 
+
+    case QUIZ_SET_STATE:
+      return {
+        ...state,
+        answerState: action.answerState,
+        results: action.results
+      }
+
+    case FINISH_QUIZ:
+      return {
+        ...state,
+        isFinished: true 
+      }
+
+    case QUIZ_NEXT_QUESTION:
+      return {
+        ...state,
+        answerState: null,
+        activeQuestion: action.number
+      }
+
+    case QUIZ_RETRY:
+      return {
+        ...state,
+        activeQuestion: 0,
+        answerState: null,
+        isFinished: false,
+        results: {},
       }  
 
     default:
